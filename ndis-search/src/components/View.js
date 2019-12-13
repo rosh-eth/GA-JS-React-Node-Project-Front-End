f325cce68e129a830e58c744c17fbb24a05e70b0
RGhadamian <roshan@ghadamian.com>
2019-12-14 10:55:14 +1100

    Fixed links for edit and delete and minor UI fixes for view page

--- ndis-search/src/components/View.js
+++ ndis-search/src/components/View.js
@@ -29,26 +29,30 @@ export class View extends React.Component {
         return (
             <React.Fragment>
                 <div class="container">
-
-                    <button class="btn btn-outline-primary mt-4 mb-4"><Link to={`/provider/create`}>Create</Link></button>
+                    <Link class="btn btn-outline-primary mt-4 mb-4 mr-3" to={`/provider/`}>Back to Providers</Link>
+                    <Link class="btn btn-primary mt-4 mb-4" to={`/provider/create`}>Create</Link>
                     <div class="card">
                         <div class="card-body">
                             
-                            {/* <h5 class="card-title d-inline">{this.state.providerDetail.name}</h5><Link to={`/provider/${providerDetail._id}/edit`}>Edit</Link> */}
-                            
-                            <h6 class="card-subtitle">{this.state.providerDetail.address}</h6>
-                            <p>{this.state.providerDetail.email}</p>
-                            <p>{this.state.providerDetail.phone}</p>
-
+                            <h5 class="card-title d-inline">{this.state.providerDetail.name}</h5><Link class="float-right" to={`/provider/${this.state.providerDetail._id}/edit`}>Edit</Link>
+                            <hr />
+                            <h6 class="card-subtitle">Company Details</h6>
+                            <div class="card-body">
+                            <p class="card-text">Address: {this.state.providerDetail.address}</p>
+                            <p class="card-text">Email: {this.state.providerDetail.email}</p>
+                            <p class="card-text">Phone: {this.state.providerDetail.phone}</p>
+                            <hr />
+                            <h6 class="card-subtitle">Services</h6>
                             <ul>
                                 {this.state.providerDetail.services.map((service) => {
                                     return <li>{service}</li>
                                 })}
+
                             </ul>
+                            </div>
 
 
-
-                            {/* <button class="btn btn-danger float-right"><Link to={`/provider/${providerDetail._id}/delete`}>Delete</Link></button> */}
+                            <Link class="btn btn-danger float-right" to={`/provider/${this.state.providerDetail._id}/delete`}>Delete</Link>
                         </div>
                     </div>
                 </div>
