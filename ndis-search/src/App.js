f6f1733177a64dd4c6b7bfeb649860bf995d8065
RGhadamian <roshan@ghadamian.com>
2020-01-25 16:37:14 +1100

    Created a provider for login

--- ndis-search/src/App.js
+++ ndis-search/src/App.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import './App.css';
 import {Form} from './components/Form';
 import {ShowAll} from './components/ShowAll';
 import {View} from './components/View';
 import {Footer} from './components/Footer';
 import Home from './components/Home';
-import {Login} from './components/Login'
+import {Login} from './components/Login';
 
-import {Switch, Route, Link} from "react-router-dom";
-import {Nav} from './components/Nav';
+import {Switch, Route} from "react-router-dom";
+import Nav from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const LoginContext = React.createContext();
+const LoginProvider = LoginContext.Provider;
+
 function App() {
+
+  const [loginState, setLoginState] = useState(false);
+  const [userProfile, setUserProfile] = useState(null);
+
   return (
   <div>
-    <Nav />
-    <Switch>
-      <Route exact path={"/provider/create"} component={Form} />
-      <Route exact path={"/provider"} component={ShowAll} />
-      <Route exact path={"/provider/edit/:edit"} component={Form} />
-      <Route exact path={"/provider/:id"} component={View} />
-      <Route exact path={"/"} component={Home} />
-      <Route exact path={"/login"} component={Login} />
-    </Switch>
-    <Footer />
+    <LoginProvider value={{loginState, setLoginState, userProfile, setUserProfile}}>
+      <Nav />
+
+      <Switch>
+        <Route exact path={"/provider/create"} component={Form} />
+        <Route exact path={"/provider"} component={ShowAll} />
+        <Route exact path={"/provider/edit/:edit"} component={Form} />
+        <Route exact path={"/provider/:id"} component={View} />
+        <Route exact path={"/"} component={Login} />
+        <Route exact path={"/login"} component={Login} />
+      </Switch>
+      <Footer />
+    </LoginProvider>
   </div>
 
   );
 }
 
+export {LoginContext};
 export default App;
